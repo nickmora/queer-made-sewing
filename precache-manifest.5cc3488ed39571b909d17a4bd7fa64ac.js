4f1dcfba8c7933162b4333d65a1010d5e04e1e02
Nick Morales <bick.norales@gmail.com>
2020-03-08 13:01:43 -0700

    Updates

---
+++ precache-manifest.5cc3488ed39571b909d17a4bd7fa64ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0fe48a89e4b38ee548d74f4c81ffd2",
+    "url": "/queer-made-sewing/index.html"
+  },
+  {
+    "revision": "03a7b1b6e70c34192c66",
+    "url": "/queer-made-sewing/static/css/2.304ff054.chunk.css"
+  },
+  {
+    "revision": "3a3f4cc7cfd6d31d0fbd",
+    "url": "/queer-made-sewing/static/css/main.9dce1ad7.chunk.css"
+  },
+  {
+    "revision": "03a7b1b6e70c34192c66",
+    "url": "/queer-made-sewing/static/js/2.186c69c9.chunk.js"
+  },
+  {
+    "revision": "1ac54127de8f16568ba12262b3294c30",
+    "url": "/queer-made-sewing/static/js/2.186c69c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3f4cc7cfd6d31d0fbd",
+    "url": "/queer-made-sewing/static/js/main.6f57e594.chunk.js"
+  },
+  {
+    "revision": "650d9a9776cb002279ec",
+    "url": "/queer-made-sewing/static/js/runtime-main.3ff1c5aa.js"
+  },
+  {
+    "revision": "d436f2a419624dbf03665f5e36168df3",
+    "url": "/queer-made-sewing/static/media/QM_logo_2.d436f2a4.svg"
+  }
+]);
